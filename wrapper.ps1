d4fe554a67be4b374735be8b060b707b7a68985a
Jan-T. Brinkmann <ppp1pp1p1@googlemail.com>
2019-01-17 16:42:01 +0100

    update git URL in wrapper.ps1

--- wrapper.ps1
+++ wrapper.ps1
@@ -3,6 +3,6 @@ Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 iex (New-Object System.Net.WebClient).downloadstring('https://get.scoop.sh')
 scoop install sudo 7zip
 
-iwr https://gist.github.com/JTBrinkmann/f86d52fe8dd8e76331fa4050e5fcbcbc/archive/master.zip -O setup.zip
+iwr https://github.com/JTBrinkmann/windows-setup/archive/master.zip -O setup.zip
 7z e .\setup.zip -osetup
 sudo setup/setup.ps1
