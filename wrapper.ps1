6f45bf16e667aa481ee53cdcfdf84584aa0149a6
Jan-T. Brinkmann <ppp1pp1p1@googlemail.com>
2019-02-06 12:14:44 +0100

    rewrote wrapper.ps1 (removed dependencies)

--- wrapper.ps1
+++ wrapper.ps1
@@ -1,8 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-iex (New-Object System.Net.WebClient).downloadstring('https://get.scoop.sh')
-scoop install sudo 7zip
-
-iwr https://github.com/JTBrinkmann/windows-setup/archive/master.zip -O setup.zip
-7z e .\setup.zip -osetup
-sudo setup/setup.ps1
+iwr https://github.com/JTBrinkmann/windows-setup/archive/master.zip -O .\setup.zip
+$shell = new-object -com shell.application
+Expand-Archive .\setup.zip .
+start -verb runas powershell "-noprofile -c $PWD\windows-setup-master\setup.ps1; pause"
